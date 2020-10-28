9509b047bffe5d7c11cfd0adc741912b4797c5d5
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-28 20:40:04 -0300

    Adjusts in layouts css

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,31 @@ import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/Aleatorio'
 import Card from './components/layout/Card'
+import './App.css'
 
 export default function App() {
     return (
-        <div id="app">
-            <h1>Fundamentos</h1>
-            <Card titulo='#04 - Desafio Aleatório'>
-                <Aleatorio min={1} max={60} />
-            </Card>
-            <Card titulo='#03 - Fragmento'>
-                <Fragmento />
-            </Card>
-            <Card titulo='#02 - Com Parametros'>
-                <ComParametro
-                    titulo="Situação do Aluno"
-                    aluno="Pedro"
-                    nota={9.3}
-                />
-            </Card>
-            <Card titulo='#01 - Primeiro Component'>
-                <Primeiro />
-            </Card>
+        <div className="App">
+            <h1>Fundamentos React</h1>
+            <div className='Cards'>
+
+                <Card titulo='#04 - Desafio Aleatório'>
+                    <Aleatorio min={1} max={60} />
+                </Card>
+                <Card titulo='#03 - Fragmento'>
+                    <Fragmento />
+                </Card>
+                <Card titulo='#02 - Com Parametros'>
+                    <ComParametro
+                        titulo="Situação do Aluno"
+                        aluno="Pedro"
+                        nota={9.3}
+                    />
+                </Card>
+                <Card titulo='#01 - Primeiro Component'>
+                    <Primeiro />
+                </Card>
+            </div>
         </div>
     )
 }
