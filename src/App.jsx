4ba49589b5f2982939304642f62b6e7a4c7233b0
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-28 21:31:02 -0300

    Passing props of father to child and use React.cloneElement function

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,20 @@ import Aleatorio from './components/Aleatorio'
 import Card from './components/layout/Card'
 import './App.css'
 import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 
 export default function App() {
     return (
         <div className="App">
             <h1>Fundamentos React</h1>
             <div className='Cards'>
-                    <Card titulo="#05 - Componente com Filhos" color='#00C8F8'>
-                        <Familia sobrenome='Ferreira' />
-                    </Card>
+                <Card titulo="#05 - Componente com Filhos" color='#00C8F8'>
+                    <Familia sobrenome='Silva' >
+                        <FamiliaMembro nome='Pedro' />
+                        <FamiliaMembro nome='Ana' />
+                        <FamiliaMembro nome='Gustavo' />
+                    </Familia>
+                </Card>
                 <Card titulo='#04 - Desafio Aleatório' color='#FA6900'>
                     <Aleatorio min={1} max={60} />
                 </Card>
--- src/components/basicos/Familia.jsx
+++ src/components/basicos/Familia.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from 'react'
-import FamiliaMembro from './FamiliaMembro'
+import React, { cloneElement } from 'react'
 
 export default props => {
+    console.log(typeof props.children.map)
     return (
         <div>
-            <FamiliaMembro nome='Pedro' sobrenome={props.sobrenome} />
-            <FamiliaMembro nome='Ana' {...props} />
-            <FamiliaMembro nome='Gustavo' sobrenome='Silva' />
+            {
+                props.children.map((child, i) => {
+                    return cloneElement(child, { ...props, key: i })
+                })
+            }
         </div>
     )
 }
