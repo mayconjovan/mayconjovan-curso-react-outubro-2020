b4ae7cc9e6d84808803c798fb4b61fa0f1c79167
Maycon J Pereira <mayconjovan@gmail.com>
2020-10-31 15:44:22 -0300

    Communication from father to child

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,16 @@ import ListaAlunos from './components/repeticao/ListaAlunos'
 import TabelaProdutos from './components/repeticao/TabelaProdutos'
 import ParOuImpar from './components/condicional/ParOuIMpar'
 import UsuarioInfo from './components/condicional/UsuarioInfo'
+import DiretaPai from './components/comunicacao/DiretaPai'
 
 export default function App() {
     return (
         <div className="App">
             <h1>Fundamentos React</h1>
             <div className='Cards'>
+                <Card titulo="#09 - Comunicação direta" color='#59323C'>
+                    <DiretaPai></DiretaPai>
+                </Card>
 
                 <Card titulo="#08 - Renderização Condicional" color='#982395'>
                     <ParOuImpar numero={20} />
---
+++ src/components/comunicacao/DiretaFilho.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default (params) => {
+    return(
+        <div>
+            <span>{params.nome} </span>
+            <span><strong>{params.idade} </strong></span>
+            <span>{params.nerd ? 'Verdadeiro' : 'Falso'}!</span>
+        </div>
+    )
+}
